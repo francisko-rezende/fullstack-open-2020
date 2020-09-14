4460595ef9683a0e0b9090f1db5e3ab0260f197b
Francisko de Moraes  Rezende <francisko.rezende@gmail.com>
2020-09-14 20:16:51 +0200

    removed old code in comments from courseinfo app

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -54,7 +54,7 @@ const App = () => {
 				exercises: 14
 			}
 		]
-	}
+	};
 
 	return (
 		<div>
@@ -62,31 +62,7 @@ const App = () => {
 			<Content parts={course.parts} />
 			<Total parts={course.parts} />
 		</div>
-	)
-
-	// const course = 'Half Stack application development';
-	// const parts = [
-	// 	{
-	// 		name: 'Fundamentals of React',
-	// 		exercises: 10
-	// 	},
-	// 	{
-	// 		name: 'Using props to pass data',
-	// 		exercises: 7
-	// 	},
-	// 	{
-	// 		name: 'State of a component',
-	// 		exercises: 14
-	// 	}
-	// ];
-
-	// return (
-	// 	<div>
-	// 		<Header course={course} />
-	// 		<Content parts={parts} />
-	// 		<Total parts={parts} />
-	// 	</div>
-	// );
+	);
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
