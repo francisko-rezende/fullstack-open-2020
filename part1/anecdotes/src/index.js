389774c2d4ffed42a3e192659cc890fff2bcfd26
Francisko de Moraes  Rezende <francisko.rezende@gmail.com>
2020-09-26 21:04:13 +0200

    finished exercise 1.13

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,38 +1,48 @@
-import React, { useState} from 'react'
-import ReactDOM from 'react-dom'
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 
 const anecdotes = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
+	'If it hurts, do it more often',
+	'Adding manpower to a late software project makes it later!',
+	'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+	'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+	'Premature optimization is the root of all evil.',
+	'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+];
+
+// const voteNumber = new Array(6).fill(0);
 
 const Button = (props) => {
-  return (
-    <div>
-      <button onClick={props.handle}>{props.text}</button>
-    </div>
-  )
-}
+	return (
+		<div>
+			<button onClick={props.handle}>{props.text}</button>
+		</div>
+	);
+};
 
 const App = (props) => {
-  const [selected, setSelected] = useState(0)
+	const [ selected, setSelected ] = useState(0);
+	const [ votes, countVotes ] = useState(Array(6).fill(0));
 
-  const handleNewAnecdote = () => {
-    setSelected(Math.floor(Math.random() * 6))
-  }
+	const handleNewAnecdote = () => setSelected(Math.floor(Math.random() * 6));
 
-  return (
-    <div>
-      {props.anecdotes[selected]}
-        <Button handle={handleNewAnecdote} text="next anecdote" />
-    </div>
-  )
-}
-ReactDOM.render(
-  <App anecdotes={anecdotes} />,
-  document.getElementById('root')
-)
+	const handleVotes = () => {
+		const votesCopy = [ ...votes ];
+    votesCopy[selected] += 1;
+    countVotes(votesCopy)
+	};
+
+	return (
+		<div>
+			{props.anecdotes[selected]}
+			<div>
+        has {votes[selected]} votes
+      </div>
+			<div>
+        <Button handle={handleVotes} text="vote" />
+        			<Button handle={handleNewAnecdote} text="next anecdote" />
+      </div>
+		</div>
+	);
+};
+ReactDOM.render(<App anecdotes={anecdotes} />, document.getElementById('root'));
