801b018ae6222efa29ee330ea4a3244a44e0383f
Francisko de Moraes  Rezende <francisko.rezende@gmail.com>
2020-09-27 18:30:28 +0200

    finished exercises 2.2 and 2.3

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -17,11 +17,20 @@ const Content = ({ course }) => {
 	return <div>{course.parts.map((part) => <Part key={part.id} part={part} />)}</div>;
 };
 
+const Total = ({ course }) => {
+	return (
+		<div>
+			<b>total of {course.parts.reduce((sum, part) => sum + part.exercises, 0)} exercises</b>
+		</div>
+	);
+};
+
 const Course = ({ course }) => {
 	return (
 		<div>
 			<Header course={course} />
 			<Content course={course} />
+			<Total course={course} />
 		</div>
 	);
 };
